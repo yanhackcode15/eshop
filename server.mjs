10748b08974c572cf7aa994ef87f5c2db6f6562a
yanhackcode15 <iamyanhu@gmail.com>
2023-02-09 19:48:45 -0800

    change port from 8000 to process env

--- server.mjs
+++ server.mjs
@@ -57,8 +57,7 @@ server.post('/signin', async (req, res)=>{
     .catch(e=>res.status(406).send(e.code))
 })
 
-
-server.listen(PORT, ()=>console.log('App running on port, ', process.env.PORT||8000))
+server.listen(process.env.PORT||8000, ()=>console.log('App running on port, ', process.env.PORT||8000))
 
 
 
